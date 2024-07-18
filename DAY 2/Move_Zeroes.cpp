021b14609d371f759361063f71dc03fcf14642be
Akanksha Verma <akanksha20062001@gmail.com>
2024-07-18 20:38:33 +0530

    Day 2 work

---
+++ DAY 2/Move_Zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size(),temp;
+    for(int i=0,j=1;i<n,j<n;i++,j++)
+    {
+        if(nums[i]==0&&nums[j]!=0)
+        {
+            temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+        else if(nums[i]==0&&nums[j]==0)
+        {
+            i=i-1;
+        }
+    }
+    }
+};
---
+++ DAY 2/Sort_Colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i,j,temp1;
+        for(i=0;i<nums.size();i++)
+        {
+            for(j=0;j<nums.size();j++)
+            {
+                if(nums[i]<nums[j])
+                {
+                    temp1=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp1;
+                }
+            }
+        }
+    }
+};
